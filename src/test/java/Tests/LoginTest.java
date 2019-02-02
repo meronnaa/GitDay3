f4ef3dafda2da176db2bb9a0820902c3ee61be85
meronnaa <46093321+meronnaa@users.noreply.github.com>
2019-02-01 22:20:31 -0500

    updating some string title

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -28,7 +28,8 @@ public class LoginTest {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
-        Assert.assertEquals(driver.getTitle(),"Web Orders");
+        String title =driver.getTitle().getText();
+        Assert.assertEquals(title,"Web Orders");
     }
 
     @Test
@@ -37,8 +38,9 @@ public class LoginTest {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
         driver.findElement(By.id("ctl00_logout")).click();
-
-        Assert.assertEquals(driver.getTitle(),"Web Orders login");
+       String title =driver.getTitle().getText();
+        Assert.assertEquals(title,"Web Orders");
+        
     }
 @AfterMethod
     public void cleanUp(){
