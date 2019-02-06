666aa33ece4362bdc62333911b5939f1af698f91
merry teweldemedhen <meronaa26@gmail.com>
2019-02-06 11:39:10 -0500

    add

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -28,7 +28,7 @@ public class LoginTest {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
-        String title =driver.getTitle().getText();
+        String title =driver.getTitle();
         Assert.assertEquals(title,"Web Orders");
     }
 
@@ -38,7 +38,7 @@ public class LoginTest {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+Keys.ENTER);
         driver.findElement(By.id("ctl00_logout")).click();
-       String title =driver.getTitle().getText();
+       String title =driver.getTitle();
         Assert.assertEquals(title,"Web Orders");
         
     }
